975951b74f2892238383d9ce83dcbad0e3f14973
Mikolaj Semeniuk <m.semeniuk@icloud.com>
2021-06-06 15:35:59 +0200

    git

---
+++ app/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace app.Data
+{
+    public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions options) : base(options)
+		{
+		}
+        
+	}
+}
